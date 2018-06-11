faa203a1b5f45df0eea49496cdddeda4373d48a3
OskarPihlak <oskar.pihlak@gmail.com>
2018-06-12 00:20:49 +0300

    Login forms

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,16 @@
 console.log('Costum Front-end code operational! Procceeding further executions commander...');
+let i = 0;
 function toggleOrderingMenu(){
     document.querySelector('.ordering').classList.toggle('hiddendiv');
+}
+function toggleLogin(){
+    document.querySelector('.login-form').classList.toggle('hiddendiv');
+}
+function toggleSignUp(){
+    document.querySelector('.signup-form').classList.toggle('hiddendiv');
+}
+function moreThings(){
+    i++;
+    console.log('Something more happened ' + i + ' times');
+
 }
